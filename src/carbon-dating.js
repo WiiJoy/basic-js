e7dba9e4eced7c851bbdfb459c8aead1539aa2ce
WiiJoy MacOS <wiijoy@bk.ru>
2022-03-13 16:33:00 +0300

    feat: done carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  console.log('sampleActivity: ', sampleActivity)
+
+  // if (typeof sampleActivity !== 'string' || sampleActivity.length === 0 || +sampleActivity > 15 || +sampleActivity <= 0) return false
+  if (isNaN(sampleActivity) || typeof sampleActivity !== 'string' || sampleActivity.length === 0 || +sampleActivity > 15 || +sampleActivity <= 0) return false
+  return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD))
+
+
 }
 
 module.exports = {
