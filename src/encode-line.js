c075397127356829cc9928b96a94d8211359a219
WiiJoy MacOS <wiijoy@bk.ru>
2022-03-14 15:04:22 +0300

    feat: done encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,8 +10,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+
+  const arr = []
+  let string = ''
+  
+  for (let letter of str) {
+    arr.push([1, letter])
+  }
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i+1] && arr[i+1][1] === arr[i][1]) {
+      arr[i][0]++
+      arr.splice(i+1, 1)
+      i--
+    }
+  }
+
+  arr.forEach(item => {
+    if (item[0] === 1) {
+      item[0] = ''
+    }
+    string = string + item.join('')
+  })
+
+  return string
+
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
