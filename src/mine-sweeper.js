6d445c7d6d69b9cf48eb9778f0ad04a85c8c53ce
WiiJoy MacOS <wiijoy@bk.ru>
2022-03-15 15:43:06 +0300

    feat: done mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let mines = []
+
+  for (let i = 0; i < matrix.length; i++) {
+    mines.push([])
+    for (let j = 0; j < matrix[i].length; j++) {
+      mines[i][j] = 0
+      if (i + 1 < matrix.length && matrix[i+1][j]) mines[i][j]++
+      if (i + 1 < matrix.length && j + 1 < matrix[i].length && matrix[i+1][j+1]) mines[i][j]++
+      if (i + 1 < matrix.length && matrix[i][j+1]) mines[i][j]++
+      if (i - 1 >= 0 && matrix[i-1][j]) mines[i][j]++
+      if (i - 1 >= 0 && j - 1 >= 0 && matrix[i-1][j-1]) mines[i][j]++
+      if (j - 1 >= 0 && matrix[i][j-1]) mines[i][j]++
+
+      if (i + 1 < matrix.length && j - 1 >= 0 && matrix[i+1][j-1]) mines[i][j]++
+
+      if (i - 1 >= 0 && j + 1 < matrix[i].length && matrix[i-1][j+1]) mines[i][j]++
+    }
+  }
+
+  return mines
+  
 }
 
 module.exports = {
