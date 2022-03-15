a57a12e3f62436ce894a7662522cd6be93fd4b8d
WiiJoy MacOS <wiijoy@bk.ru>
2022-03-15 10:59:16 +0300

    feat: done extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,8 +15,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let string = ''
+
+  console.log(options)
+
+  // if (!options.hasOwnProperty(repeatTimes)) return str + options.addition
+
+  if (!('repeatTimes' in options)) return str + options.addition
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    string += str
+    if ('addition' in options) {
+      string += options.addition
+      if ('additionRepeatTimes' in options) {
+        let additionString = ''
+        for (let j = 1; j < options.additionRepeatTimes; j++) {
+          if ('additionSeparator' in options) {
+            additionString = additionString + String(options.additionSeparator) + options.addition
+          } else {
+            additionString = additionString + '|' + options.addition
+          }
+
+
+
+          // string = ('additionSeparator' in options) ? string + String(options.additionSeparator) : string
+        }
+
+        string += additionString
+
+      }
+    }
+    if (i !== (options.repeatTimes - 1)) string = ('separator' in options) ? string + String(options.separator) : string + '+'
+  }
+
+  return string
+
+
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
