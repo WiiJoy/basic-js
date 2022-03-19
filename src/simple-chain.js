a9a9a94b6686de49cfaf15eda1fab469e6228eb0
WiiJoy MacOS <wiijoy@bk.ru>
2022-03-19 18:25:35 +0300

    feat: done simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.chain.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.chain.push(value !== null ? value : null)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (typeof position !== 'number' || position <= 0 || position - 1 >= this.chain.length ) {
+      this.chain = []
+      throw new Error ('You can\'t remove incorrect link!')
+    }
+    this.chain.splice(position - 1, 1)
+    return this
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    let finalChain = ''
+      for (let i = 0; i < this.chain.length; i++) {
+        if (i === 0) {
+          finalChain += `( ${this.chain[i]} )`
+        } else {
+          finalChain += `~~( ${this.chain[i]} )`
+        }
+      }
+    
+
+
+
+    // let finalChain = this.chain
+    this.chain = []
+
+    return finalChain
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 };
