c4146d98d083f5935ae3053b8bf96422bb7bdaf6
WiiJoy MacOS <wiijoy@bk.ru>
2022-03-21 09:28:19 +0300

    done vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool = true) {
+    this.bool = bool
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  encrypt(str, key) {
+    // const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let currAbc = abc
+    // let str = 'attack at dawn!'
+    // let key = 'alphonse'
+    if (!str || !key) throw new Error ('Incorrect arguments!')
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    str = str.toUpperCase()
+    key = key.toUpperCase()
+    let codeStr = ''
+
+    while (key.length < str.length) {
+      key = key.concat(key)
+    }
+
+    let j = 0
+    for (let i = 0; i < str.length; i++) {
+      if (abc.includes(str[i])) {
+        currAbc = abc.slice(abc.indexOf(key[j])) + abc.slice(0, abc.indexOf(key[j]))
+        console.log(abc, currAbc)
+        codeStr += currAbc[abc.indexOf(str[i])]
+      } else {
+        codeStr += str[i]
+        j--
+      }
+      j++
+    }
+
+    return this.bool ? codeStr : codeStr.split('').reverse().join('')
+
+  }
+  decrypt(str, key) {
+    let currAbc = abc
+    if (!str || !key) throw new Error ('Incorrect arguments!')
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+
+    str = str.toUpperCase()
+    key = key.toUpperCase()
+    let decodeStr = ''
+
+    while (key.length < str.length) {
+      key = key.concat(key)
+    }
+
+    let j = 0
+    for (let i = 0; i < str.length; i++) {
+      if (abc.includes(str[i])) {
+        currAbc = abc.slice(abc.indexOf(key[j])) + abc.slice(0, abc.indexOf(key[j]))
+        console.log(abc, currAbc)
+        decodeStr += abc[currAbc.indexOf(str[i])]
+      } else {
+        decodeStr += str[i]
+        j--
+      }
+      j++
+    }
+
+    console.log(decodeStr)
+
+    return this.bool ? decodeStr : decodeStr.split('').reverse().join('')
+
   }
 }
 
